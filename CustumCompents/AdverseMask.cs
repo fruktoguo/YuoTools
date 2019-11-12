4645b1c321c2b8d296fc03a359f68fc66798ee6e
YuoHira <fruktoyao@gmail.com>
2019-11-12 15:56:59 +0800

    no message

---
+++ CustumCompents/AdverseMask.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AdverseMask : Mask
+{
+    public override bool IsRaycastLocationValid(Vector2 sp, Camera eventCamera)
+    {
+        if (isActiveAndEnabled)
+            return true;
+        return !RectTransformUtility.RectangleContainsScreenPoint(rectTransform, sp, eventCamera);
+    }
+}
