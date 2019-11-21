52911f152ae9454eba88bbfd74269a5a9f677f5d
YuoHira <fruktoyao@gmail.com>
2019-11-21 13:41:52 +0800

    no message

---
+++ Collect/ShowOfDelay.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+namespace YuoTools
+{
+    public class ShowOfDelay : MonoBehaviour
+    {
+        public float Delay = 3;
+        bool Trigger = true;
+        void OnEnable()
+        {
+            Trigger = !Trigger;
+            if (Trigger)
+            {
+                return;
+            }
+            gameObject.Hide();
+            this.YuoDelay(() => gameObject.Show(), Delay.Clamp());
+        }
+    }
+}
